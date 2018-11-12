b3b4c8508a4f5e51448559dcdea262df08931936
Max Couch <caxmouch@gmail.com>
2018-11-12 11:46:38 -0500

    completing exercises 1 - 4, but plus conditions are not met

--- week-b-fizzbuzz.js
+++ week-b-fizzbuzz.js
@@ -6,7 +6,21 @@
     Each multiple of both 3 and 5 replaced with 'fizzbuzz'
 */
 function fizzBuzz1(){
-
+    let num = 1;
+    let num3 = "fizz";
+    let num5 = "buzz";
+    let num35 = "fizzbuzz";
+    for (let i = 1; i <= 100; i++){
+        num = i;
+        if (i % 3 === 0 && i % 5 === 0) {
+            num = num35;
+        } else if (i % 3 === 0) {
+            num = num3;
+        } else if (i % 5 === 0) {
+            num = num5;
+        }
+        console.log(num)
+    }
 }
 
 /*
@@ -18,7 +32,21 @@ function fizzBuzz1(){
     Validation that m <= n is a plus. 
 */
 function fizzBuzz2(m,n){
-
+    let num = 1;
+    let num3 = "fizz";
+    let num5 = "buzz";
+    let num35 = "fizzbuzz";
+    for (let i = m; i <= n; i++){
+        num = i;
+        if (i % 3 === 0 && i % 5 === 0) {
+            num = num35;
+        } else if (i % 3 === 0) {
+            num = num3;
+        } else if (i % 5 === 0) {
+            num = num5;
+        }
+        console.log(num)
+    }
 }
 
 /*
@@ -29,8 +57,19 @@ function fizzBuzz2(m,n){
     Each multiple of both fizzNum and BuzzNum replaced with 'fizzbuzz'
     Validation that m <= n is a plus. 
 */
-function fizzBuzz4(m,n,fizzNum,buzzNum){
-
+function fizzBuzz3(m,n,fizzNum,buzzNum){
+    let num = 1;
+    for (let i = m; i <= n; i++){
+        num = i;
+        if (i % fizzNum === 0 && i % buzzNum === 0) {
+            num = 'fizzbuzz';
+        } else if (i % fizzNum === 0) {
+            num = 'fizz';
+        } else if (i % buzzNum === 0) {
+            num = 'buzz';
+        }
+        console.log(num)
+    }
 }
 
 /*
@@ -42,8 +81,21 @@ function fizzBuzz4(m,n,fizzNum,buzzNum){
     Validation the m <= n is a plus. 
     The arrays may have any length, but validation that their length is the SAME is a plus. 
 */
-function fizzBuzz3(m,n,numbers,terms){
-
+function fizzBuzz4(m,n,numbers,terms){
+    let num = 1;
+    /*let numbers = [x,y];
+    let terms = ["a","b","c"];*/
+    for (let i = m; i <= n; i++){
+        num = i;
+        if (i % numbers[0] === 0 && i % numbers[1] === 0) {
+            num = terms[2];
+        } else if (i % numbers[0] === 0) {
+            num = terms[0];
+        } else if (i % numbers[1] === 0) {
+            num = terms[1];
+        }
+        console.log(num)
+    }
 }
 
 
