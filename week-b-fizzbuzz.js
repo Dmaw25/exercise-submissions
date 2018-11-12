fdbf72e9b20ec9813f49ec8bc6e0d252eda7722d
Stewart Gardner <StewartBenGardner@gmail.com>
2018-11-12 10:31:36 -0500

    Exercises 1 & 2 completed.

--- week-b-fizzbuzz.js
+++ week-b-fizzbuzz.js
@@ -6,9 +6,21 @@
     Each multiple of both 3 and 5 replaced with 'fizzbuzz'
 */
 function fizzBuzz1(){
-
+    for (let i=1;i<101;++i){
+        if(i%3==0 && i%5==0){
+            console.log("fizzbuzz")
+        }
+        else if(i%3==0){
+            console.log("fizz");
+        }
+        else if(i%5==0){
+            console.log("buzz");;
+        }
+        else {
+            console.log(i);
+        }
+    }
 }
-
 /*
     Variable-length FizzBuzz:
     Write a function which prints out the numbers m-n, with the following substitutions:
@@ -18,7 +30,20 @@ function fizzBuzz1(){
     Validation that m <= n is a plus. 
 */
 function fizzBuzz2(m,n){
-
+    for(let i=m;i<=n;++i){
+        if(i%3==0 && i%5==0){
+            console.log("fizzbuzz")
+        }
+        else if(i%3==0){
+            console.log("fizz");
+        }
+        else if(i%5==0){
+            console.log("buzz");;
+        }
+        else {
+            console.log(i);
+        }
+    }
 }
 
 /*
