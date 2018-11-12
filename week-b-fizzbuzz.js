9dc025f171500dacb955d8911ae7a54e5afea9ec
unknown <cbejar93@gmail.com>
2018-11-12 11:46:00 -0500

    fizzbuzz craziness

--- week-b-fizzbuzz.js
+++ week-b-fizzbuzz.js
@@ -7,6 +7,16 @@
 */
 function fizzBuzz1(){
 
+    for(i=1; i <100;i++){
+        if (i % 3 === 0 && i % 5 === 0){
+            console.log(" fizz ");
+        }else if (i % 5 ===0 ){
+            console.log(" buzz ");
+        }else if (i % 3 ===0){
+            console.log(" fizzbuzz ");
+        }else{console.log(i)}
+    }
+
 }
 
 /*
@@ -18,6 +28,20 @@ function fizzBuzz1(){
     Validation that m <= n is a plus. 
 */
 function fizzBuzz2(m,n){
+
+    for (i=1; i<100; i++){
+        if ( m>n){
+            console.log(m +" is larger than "+ n+" error");
+        }else if  (i >= m && i<=n){
+            if (i % 3 === 0 && i % 5 === 0){
+                console.log(" fizz ");
+            }else if (i % 5 ===0 ){
+                console.log(" buzz ");
+            }else if (i % 3 ===0){
+                console.log(" fizzbuzz ");
+            }else{console.log(i)}
+        }
+    }
 
 }
 
@@ -31,6 +55,20 @@ function fizzBuzz2(m,n){
 */
 function fizzBuzz4(m,n,fizzNum,buzzNum){
 
+    for (i=1; i<100; i++){
+        if ( m>n){
+            console.log(m +" is larger than "+ n+" error");
+        }else if  (i >= m && i<=n){
+            if (i % fizzNum === 0 && i % buzzNum === 0){
+                console.log(" fizz ");
+            }else if (buzzNum % 5 ===0 ){
+                console.log(" buzz ");
+            }else if (fizzNum % 3 ===0){
+                console.log(" fizzbuzz ");
+            }else{console.log(i)}
+        }
+    }
+
 }
 
 /*
@@ -43,6 +81,21 @@ function fizzBuzz4(m,n,fizzNum,buzzNum){
     The arrays may have any length, but validation that their length is the SAME is a plus. 
 */
 function fizzBuzz3(m,n,numbers,terms){
+
+    for (i=1; i<100; i++){
+        if ( m>n){
+            console.log(m +" is larger than "+ n+" error");
+        }else if  (i >= m && i<=n){
+            if (numbers.length === terms.length){
+                for (j = 0; numbers.length; j++){
+                    if (i % numbers[j] === 0){
+
+                        console.log(terms[j]);
+                    }else{console.log(i);}
+                }
+            }else{console.log("arrays two different lengths cannot compute!!!");}
+        }
+    }
 
 }
 
