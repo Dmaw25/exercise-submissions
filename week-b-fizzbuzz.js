e56a2a3ff21d32dae9b27da5b7f3e53fae7b30ec
Jonathan Snider <jonathan.snider2006@gmail.com>
2018-11-12 15:03:22 -0500

    FizzBuzz submission

--- week-b-fizzbuzz.js
+++ week-b-fizzbuzz.js
@@ -7,7 +7,24 @@
 */
 function fizzBuzz1(){
 
-    console.log('fizzbuzz1');
+    for (i=1;i<101;i++)
+
+    if( i%3==0 && i%5==0)
+    {
+        console.log("fizzbuzz");
+    }
+    else if (i%3==0)
+    {
+        console.log("fizz");
+    }
+    else if (i%5==0)
+    {
+        console.log("buzz");
+    }
+    else
+    {
+        console.log(i);
+    }
 
 }
 
@@ -20,7 +37,30 @@ function fizzBuzz1(){
     Validation that m <= n is a plus. 
 */
 function fizzBuzz2(m,n){
+    if (m>n)
+    {
+        return("m is greater than n so this really doesn't work");
+    }
 
+    for (i=m;i<(n+1);i++)
+    {
+        if( i%3==0 && i%5==0)
+        {
+            console.log("fizzbuzz");
+        }
+        else if (i%3==0)
+        {
+            console.log("fizz");
+        }
+        else if (i%5==0)
+        {
+            console.log("buzz");
+        }
+        else
+        {
+            console.log(i);
+        } 
+    }
 }
 
 /*
@@ -32,7 +72,30 @@ function fizzBuzz2(m,n){
     Validation that m <= n is a plus. 
 */
 function fizzBuzz3(m,n,fizzNum,buzzNum){
+    if (m>n)
+    {
+        return("m is greater than n so this really doesn't work");
+    }
 
+    for (i=m;i<(n+1);i++)
+    {
+        if( i%fizzNum==0 && i%buzzNum==0)
+        {
+            console.log("fizzbuzz");
+        }
+        else if (i%fizzNum==0)
+        {
+            console.log("fizz");
+        }
+        else if (i%buzzNum==0)
+        {
+            console.log("buzz");
+        }
+        else
+        {
+            console.log(i);
+        } 
+    }
 }
 
 /*
@@ -45,7 +108,33 @@ function fizzBuzz3(m,n,fizzNum,buzzNum){
     The arrays may have any length, but validation that their length is the SAME is a plus. 
 */
 function fizzBuzz4(m,n,numbers,terms){
+    if (m>n)
+    {
+        return("m is greater than n so this really doesn't work");
+    }
+    if(numbers.length != terms.length){
+        console.log("the length of 'numbers' and 'terms' isn't equal which may not necessarily be an issue")
+    }
 
+    for (i=m;i<(n+1);i++)
+    {
+        if( i%(numbers[0])==0 && i%(numbers[1])==0)
+        {
+            console.log(terms[0],terms[1]);
+        }
+        else if (i%(numbers[0])==0)
+        {
+            console.log(terms[0]);
+        }
+        else if (i%(numbers[1])==0)
+        {
+            console.log(terms[1]);
+        }
+        else
+        {
+            console.log(i);
+        } 
+    }
 }
 
 
