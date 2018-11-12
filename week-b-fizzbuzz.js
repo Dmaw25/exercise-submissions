d69bc9db7ac99f3699e8b3a582f36b7f73f8eb06
unknown <arabia.cw@gmail.com>
2018-11-12 11:22:52 -0500

    Cam.W

--- week-b-fizzbuzz.js
+++ week-b-fizzbuzz.js
@@ -6,8 +6,20 @@
     Each multiple of both 3 and 5 replaced with 'fizzbuzz'
 */
 function fizzBuzz1(){
-
+    for(let i=1;i<=100;i++){
+		if(i%5 === 0 && i%3 === 0){
+			print('FizzBuzz');
+		} else if(i%3 === 0){
+			print('Fizz');
+		} else if(i%5 === 0){
+			print('Buzz');
+		} else {
+			print(i);
+		}
+	}
 }
+
+
 
 /*
     Variable-length FizzBuzz:
