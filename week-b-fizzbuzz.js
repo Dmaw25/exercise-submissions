963c2d30784f22e0ed534660c393eb66feb2e574
OdinThunder <naka506@yahoo.com>
2018-11-12 12:03:38 -0500

    finish challenge

--- week-b-fizzbuzz.js
+++ week-b-fizzbuzz.js
@@ -5,10 +5,29 @@
     Each multiple of 5 replaced with 'buzz'
     Each multiple of both 3 and 5 replaced with 'fizzbuzz'
 */
+
+
+let arr=[];
 function fizzBuzz1(){
+    for ( i = 1; arr.length <100; i++) {
+        if (i % 5 === 0 && i % 3 ===0){
+            arr.push('fizzbuzz');
+        }
+        else if (i % 3 === 0){
+            arr.push('fizz');
+        }
+        else if (i % 5 === 0){
+            arr.push('buzz');
+        }
+        
+        else {
+            arr.push(i);
+        }
+       
+        console.log(arr);
 
 }
-
+}
 /*
     Variable-length FizzBuzz:
     Write a function which prints out the numbers m-n, with the following substitutions:
@@ -18,7 +37,28 @@ function fizzBuzz1(){
     Validation that m <= n is a plus. 
 */
 function fizzBuzz2(m,n){
+    var m=2;
+    var n=90;
+    for ( i = 2; arr.length <90; i++) {
+        if (i % 5 === 0 && i % 3 ===0){
+            arr.push('fizzbuzz');
+        }
+        else if (i % 3 === 0){
+            arr.push('fizz');
+        }
+        else if (i % 5 === 0){
+            arr.push('buzz');
+        }
+        
+        else {
+            arr.push(i);
+        }
+       
+        console.log(arr);
+    
 
+
+}
 }
 
 /*
@@ -29,10 +69,27 @@ function fizzBuzz2(m,n){
     Each multiple of both fizzNum and BuzzNum replaced with 'fizzbuzz'
     Validation that m <= n is a plus. 
 */
-function fizzBuzz4(m,n,fizzNum,buzzNum){
-
+function fizzBuzz4(fizzNum,buzzNum){
+var fizzNum = 3;
+var buzzNum = 5;
+for ( i = 4; arr.length <80; i++) {
+    if (i % buzzNum === 0 && i%fizzNum ===0){
+        arr.push('fizzbuzz');
+    }
+    else if (i%fizzNum ===0){
+        arr.push('fizz');
+    }
+    else if (i % buzzNum === 0){
+        arr.push('buzz');
+    }
+    
+    else {
+        arr.push(i);
+    }
+   
+    console.log(arr);
 }
-
+}
 /*
     Variable-length FizzBuzz with arrays:
     Write a function which prints out the numbers m-n with substitutions of: 
@@ -43,6 +100,26 @@ function fizzBuzz4(m,n,fizzNum,buzzNum){
     The arrays may have any length, but validation that their length is the SAME is a plus. 
 */
 function fizzBuzz3(m,n,numbers,terms){
+    var m=5;
+    var n=70;
+    for ( i = 5; arr.length <100; i++) {
+        if (i % 2 === 0 && i % 4 ===0){
+            arr.push('fizzbuzz');
+        }
+        else if (i % 3 === 0){
+            arr.push('fizz');
+        }
+        else if (i % 5 === 0){
+            arr.push('buzz');
+        }
+        
+        else {
+            arr.push(i);
+        }
+       
+        console.log(arr);
+
+}
 
 }
 
