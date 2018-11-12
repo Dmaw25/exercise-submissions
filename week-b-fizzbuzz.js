93135b01a29fa87366f040155916e774fb097946
Kaleigh Jaeger <jaegerka@dukes.jmu.edu>
2018-11-12 11:47:29 -0500

    changed file names and updated code

--- week-b-fizzbuzz.js
+++ week-b-fizzbuzz.js
@@ -5,9 +5,22 @@
     Each multiple of 5 replaced with 'buzz'
     Each multiple of both 3 and 5 replaced with 'fizzbuzz'
 */
-function fizzBuzz1(){
 
-}
+
+function fizzBuzz1(){
+    for (i=0; i<101; i++) {
+        if (i % 3 === 0 && i % 5 !== 0) {
+            console.log('fizz');
+        }
+        else if (i % 5 === 0 && i % 3 !== 0) {
+            console.log('buzz');
+        }
+        else if (i % 5 === 0 && i % 3 === 0) {
+            console.log('fizzbuzz');
+        }
+        else {console.log(i)};
+        }
+    };
 
 /*
     Variable-length FizzBuzz:
@@ -18,8 +31,28 @@ function fizzBuzz1(){
     Validation that m <= n is a plus. 
 */
 function fizzBuzz2(m,n){
+    if (m % 3 === 0 && m % 5 !== 0) {
+        console.log('fizz');
+    }
+    else if (m % 5 === 0 && m % 3 !== 0) {
+        console.log('buzz');
+    }
+    else if (m % 5 === 0 && m % 3 === 0) {
+        console.log('fizzbuzz');
+    }
+    else {console.log(m)};
 
-}
+    if (n % 3 === 0 && n % 5 !== 0) {
+        console.log('fizz');
+    }
+    else if (n % 5 === 0 && n % 3 !== 0) {
+        console.log('buzz');
+    }
+    else if (n % 5 === 0 && n % 3 === 0) {
+        console.log('fizzbuzz');
+    }
+    else {console.log(n)};
+    };
 
 /*
     Variable-length, varying-fizz/buzz number FizzBuzz:
@@ -30,8 +63,29 @@ function fizzBuzz2(m,n){
     Validation that m <= n is a plus. 
 */
 function fizzBuzz4(m,n,fizzNum,buzzNum){
+    if (m % fizzNum === 0 && m % buzzNum !== 0) {
+        console.log('fizz');
+    }
+    else if (m % buzzNum === 0 && m % fizzNum !== 0) {
+        console.log('buzz');
+    }
+    else if (m % fizzNum === 0 && m % buzzNum === 0) {
+        console.log('fizzbuzz');
+    }
+    else {console.log(m)};
 
-}
+    if (n % fizzNum === 0 && n % buzzNum !== 0) {
+        console.log('fizz');
+    }
+    else if (n % buzzNum === 0 && n % fizzNum !== 0) {
+        console.log('buzz');
+    }
+    else if (n % fizzNum === 0 && n % buzzNum === 0) {
+        console.log('fizzbuzz');
+    }
+    else {console.log(n)};
+    };
+
 
 /*
     Variable-length FizzBuzz with arrays:
@@ -43,7 +97,20 @@ function fizzBuzz4(m,n,fizzNum,buzzNum){
     The arrays may have any length, but validation that their length is the SAME is a plus. 
 */
 function fizzBuzz3(m,n,numbers,terms){
-
+for (i=m; i<n; i++) {
+    if (i % numbers[0] === 0 && i % numbers[1] !== 0 && i % numbers[2] !== 0) {
+        console.log(terms[0]);
+    }
+    else if (i % numbers[1] === 0  && i % numbers[0] !== 0 && i % numbers[2] !== 0) {
+        console.log(terms[1]);
+    }
+    else if (i % numbers[2] === 0  && i % numbers[1] !== 0 && i % numbers[0] !== 0) {
+        console.log(terms[2]);
+    }    
+    else if (i % numbers[1] === 0  && i % numbers[0] === 0 && i % numbers[2] !== 0) {
+        console.log(terms[0] + terms[1]);
+    }
+    else {console.log(i)}
 }
 
-
+}
