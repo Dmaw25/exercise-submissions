dc48f6857f2725231b2396aa0e6a266b6a033697
unknown <cbejar93@gmail.com>
2018-11-19 10:11:22 -0500

    fizzbuzz

--- week-b-fizzbuzz.js
+++ week-b-fizzbuzz.js
@@ -1,4 +1,4 @@
-/*
+g/*
     Basic FizzBuzz:
     Write a function which prints out the numbers 1-100, with the following substitutions:
     Each multiple of 3 replaced with 'fizz'
@@ -9,11 +9,11 @@ function fizzBuzz1(){
 
     for(i=1; i <100;i++){
         if (i % 3 === 0 && i % 5 === 0){
-            console.log(" fizz ");
+            console.log(" fizzbuzz ");
         }else if (i % 5 ===0 ){
             console.log(" buzz ");
         }else if (i % 3 ===0){
-            console.log(" fizzbuzz ");
+            console.log(" fizz ");
         }else{console.log(i)}
     }
 
@@ -27,7 +27,7 @@ function fizzBuzz1(){
     Each multiple of both 3 and 5 replaced with 'fizzbuzz'
     Validation that m <= n is a plus. 
 */
-function fizzBuzz2(m,n){
+function fizzBuzz2(m,n){h
 
     for (i=1; i<100; i++){
         if ( m>n){
@@ -97,6 +97,44 @@ function fizzBuzz3(m,n,numbers,terms){
         }
     }
 
+}
+
+function fizzBuzzTroy(m,n,numbers,terms){
+    var lowNum = m;
+    var highNum = n;
+    
+    var numArr = numbers;
+    var termArr = terms;
+
+    x = numArr.length;
+
+    if (lowNum > highNum){
+    
+        console.log(" ERROR!: Numbers are not in proper order, Try Again!");
+    
+    }   else if (numArr.length != termArr.length){   
+    
+            console.log("Array Lengths are not the proper size! Try Again!");
+    
+    }   else {
+
+            for (i=lowNum; i<(highNum+ 1); i++){
+                
+                num = i;
+                
+                console.log(i);
+                
+                for ( y = 0 ; y < (numArr.length +1) ; y ++){
+                    
+                    if (num % numArr[y] == 0 && num != 0){
+                        console.log(termArr[y]);
+                    }
+                } 
+
+            }
+    
+    }
+  
 }
 
 
