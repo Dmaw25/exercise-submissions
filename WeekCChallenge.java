1282a5fa2ac4c12a3fa6ff87b201ea5994b85248
ebanks <e.banks2040@gmail.com>
2018-11-19 11:59:50 -0500

    adding code challenge stuff

---
+++ WeekCChallenge.java
@@ -0,0 +1,51 @@
+public class WeekCChallenge {
+
+	public static void main(String[] args){
+		
+		//1. create a new instance of the Calculator class
+		Calculator newCalc = new Calculator();
+		
+
+		//2. call each of Calculator's instance methods: add, subtract, multiply, and divide
+		// with arguments of your choice, assigning the result to the variables below
+		
+		double additionResult;
+		double subtractionResult;
+		double multiplicationResult;
+		double divisionResult;
+
+		Calculator.newCalc("add");
+		Calculator.newCalc("subtract");
+		Calculator.newCalc("multiply");
+		Calculator.newCalc("divide");
+
+		
+
+
+		//3. print the value of each of these variables to the console with a descriptive message
+
+		System.out.println("Results of adding: "+newCalc.additionResult());
+		System.out.println("Results of subtracting: "+newCalc.subtractionResult());
+		System.out.println("Results of multiplication: "+newCalc.multiplicationResult());
+		System.out.println("Results of division: "+newCalc.divisionResult());
+
+
+
+		//4. implement the static fizzBuzz method in this class and call it from the main method
+
+	public static void fizzBuzz(int m, int n, int[] numbers, String[] terms){
+		public int m;
+		public int n;
+		public int[] numbers;
+		public String[] terms;
+
+
+
+
+
+		}
+
+		
+
+	}
+}
