48acadf1fe8fe4225bf83e5f69b4e0f0962b08ce
Uroosa <uroosafarooq86@gmail.com>
2018-11-19 16:35:14 -0500

    SPARK ASSIGNMENT

---
+++ SPARK-ASSIGNMENT/Factorial.js
@@ -0,0 +1,18 @@
+/*
+ 3. Return the factorial of n
+
+ f(0) = 1
+ f(1) = 1
+ f(3) = 6
+*/
+homework.factorial = function(n){
+
+};
+//If num = 0 or num = 1, the factorial will return 1
+if (num === 0 || num === 1)
+return 1;
+
+while (num > 1) {
+    num--; // decrementation by 1 at each iteration result = result * num; // or result *= num;
+return factorial;
+}
