28d65b9fbe1da320e549de8c103a2639b3a7bd68
Mforeman88 <Malika.Foreman@gmail.com>
2018-11-19 12:11:32 -0500

    code challenge

---
+++ weekCChallenge/Calculator.java
@@ -0,0 +1,19 @@
+
+public class Calculator {
+
+	
+        public double addition(int num1, int num2) {
+                return num1 + num2;
+        }
+
+        public double subtraction(int num1, int num2) {
+                return num1 - num2;
+        }
+        public double multiply(int num1, int num2) {
+                return num1 * num2;
+        }
+        public double divide(int num1, int num2); {
+                return num1 / num2;
+        }
+
+}
---
+++ weekCChallenge/WeekCChallenge.java
@@ -0,0 +1,59 @@
+public class WeekCChallenge extends Calculator {
+
+	public static void main(String[] args){
+		fizzbuzz();
+	//create a new instance of the Calculator class
+	
+	public Challenge() {	
+	Calculator one = new Calculator();
+	}
+
+	//call each of Calculator's instance methods: add, subtract, multiply
+	// and divide with arguments of your choice, assigning the result
+	// to the variables below
+
+		double additionResult;
+		double subtractionResult;
+		double multiplicationResult;
+		double divisionResult;
+
+	public double additionResult(num1, num2){
+		return num1 + num2;
+	}
+	public double subtractionResult(num1, num2){
+		return num1 - num2;
+	}
+	public double multiplicationResult(num1, num2){
+		return num1 * num2;
+	}
+	public double divisionResult(num1, num2){
+		return num1 / num2;
+	}
+	
+	// print the value of each of these variables to the console with a descriptive message
+		System.out.println("num1 + num2 =" + additionResult);
+		System.out.println("num1 - num2 =" + subtractionResult);
+		System.out.println("num1 * num2=" + multiplicationResullt);
+		System.out.println("num1 / num2=" + divisionResult);	
+
+	//implement the static fizzBuzz method in this class and call it from the main method
+
+	public static void fizzBuzz(int m, int n, int[] numbers, String[] terms){
+		int m = fizz
+		int n = buzz
+		
+		int count = 0;
+		while(count < 100){
+		If (count % 5 == 0){
+		System.out.println("fizz");
+		} else if (count % 10 == 0) {
+		System.out.println("buzz");
+		}
+		else (count % 5 == 0 && count % 10 == 0) {
+			System.out.print("fizzBuzz");
+		
+
+	
+	}
+}
+
