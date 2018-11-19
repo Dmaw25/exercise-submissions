0dafd332502330eb00e8bed6eb727f4ff2c78e85
njahromi <44483465+njahromi@users.noreply.github.com>
2018-11-19 11:19:45 -0500

    Created Week C Challenge Files!

---
+++ Calculator.java
@@ -0,0 +1,55 @@
+//scanners are a trick I learned over the weekend, they gather user input in the console.
+import java.util.Scanner;
+public class Calculator extends WeekCChallenge {
+static int op = 0;
+//The scanner is named natescan because I wanted to add my own personal touch, obvi
+static Scanner natescan = new Scanner(System.in);
+static double arg0;
+static double arg1;
+static double additionResult;
+static double subtractionResult;
+static double multiplicationResult;
+static double divisionResult;
+
+public static void main(String [] args){
+	do{
+		System.out.println("Welcome to Nathan's cat powered calculator.  Please select 1 for Addition. 2 for Subtraction. 3 for multiplication. 4 for divsion.");
+		op = natescan.nextInt();
+	}while(op<1 && op<4);
+	
+	if(op == 1){
+		//Kitten speak must be used here.  Don't ask questions :3
+		System.out.println("Pwease input your first number");
+		arg0 = natescan.nextInt();
+		System.out.println("Ohh thank chu! pwease give us your second number.");
+		arg1 = natescan.nextDouble();
+		additionResult = arg0 + arg1;
+		//Descriptive answer goes here!
+		System.out.println("Teh nerdy math kittens are hard at work adding up your answer! OwO They fetched... "+additionResult);
+	}
+	if(op == 2){
+		System.out.println("Pwease input your first number");
+		arg0 = natescan.nextInt();
+		System.out.println("Ohh thank chu! pwease give us your second number.");
+		arg1 = natescan.nextDouble();
+		subtractionResult = arg0 - arg1;
+		System.out.println("The Java cats are furiously pawing around java beans to find you're answer... looks like it's "+subtractionResult);
+	}
+	if(op == 3){
+		System.out.println("Pwease input your first number");
+		arg0 = natescan.nextInt();
+		System.out.println("Ohh thank chu! pwease give us your second number.");
+		arg1 = natescan.nextDouble();
+		multiplicationResult = arg0 * arg1;
+		System.out.println("Uploading math to teh interwebz... The answer is: "+multiplicationResult);
+	}
+	if(op == 4){
+		System.out.println("Pwease input your first number");
+		arg0 = natescan.nextInt();
+		System.out.println("Ohh thank chu! pwease give us your second number.");
+		arg1 = natescan.nextDouble();
+		divisionResult = arg0 / arg1;
+		System.out.println("Nomming all your browser cookies... The result is: "+divisionResult);
+	}
+}
+}
---
+++ WeekCChallenge.java
@@ -0,0 +1,50 @@
+public class WeekCChallenge {
+
+	public static void main(String[] args){
+		
+		//1. create a new instance of the Calculator class
+
+	WeekCChallenge calc = new Calculator();
+
+		//2. call each of Calculator's instance methods: add, subtract, multiply, and divide
+		// with arguments of your choice, assigning the result to the variables below
+		
+		double additionResult;
+		double subtractionResult;
+		double multiplicationResult;
+		double divisionResult;
+
+		//3. print the value of each of these variables to the console with a descriptive message
+		
+	System.out.println("Execute Calculator.java to see the answers to 1 2 and 3!");
+
+		//4. implement the static fizzBuzz method in this class and call it from the main method
+	}
+	
+
+	public static void fizzBuzz(int m, int n, int[] numbers, String[] terms){
+//function fizzBuzz4(m,n,numbers,terms){
+//if (m > n || numbers.length > terms.length || terms.length > numbers.length) {
+//    console.log ("Error!");
+//}
+
+//else {
+//    for (i = m; i <= n; i++) {
+//        termString = " ";
+//        for (y = 0; y < numbers.length; y++) {
+//            if (i % numbers[y] === 0) {
+//                termString = termString + terms[y];
+//            }
+//        }
+//        if (termString != " ") {
+//            console.log(termString);
+//        }
+//        else {
+//            console.log(i);
+//        }
+//        
+//    }
+//}
+//}
+	}
+}
