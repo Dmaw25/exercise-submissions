52824fdea8e4b91666c1ea5841e360c0145c7b51
jakemulrenin <44554162+jakemulrenin@users.noreply.github.com>
2018-11-20 07:35:43 -0500

    'Code Challenge Files, accidentally pushed to my repository yesterday'

--- Calculator.java
+++ Calculator.java
@@ -1,18 +1,18 @@
 public class Calculator {
 
 	public double add(double arg0, double arg1) {
-		return 0.0;
+		return (arg0 + arg1);
 	}
 
 	public double subtract(double arg0, double arg1) {
-		return 0.0;
+		return (arg0 - arg1);
 	}
 
 	public double multiply(double arg0, double arg1) {
-		return 0.0;
+		return (arg0 * arg1);
 	}
 
 	public double divide(double arg0, double arg1) {
-		return 0.0;
+		return (arg0 / arg1);
 	}
 }
