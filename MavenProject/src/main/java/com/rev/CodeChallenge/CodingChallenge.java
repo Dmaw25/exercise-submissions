3d6a1da91e0ee434f3a4861de52bc570118290bc
Dmaw25 <44574708+Dmaw25@users.noreply.github.com>
2018-12-10 10:19:39 -0500

    Oracle and Maven Projects

--- MavenProject/src/main/java/com/rev/CodeChallenge/CodingChallenge.java
+++ MavenProject/src/main/java/com/rev/CodeChallenge/CodingChallenge.java
@@ -9,26 +9,21 @@ public class CodingChallenge extends CodeChallengeUtility{
 		// TODO Auto-generated method stub
 		//Not gonna do so much commenting in my main method
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Input start gene: Must be 8 numbers and a config of letters ACTG:");
+		System.out.println("Input start gene: Must be 8 numbers and a config of letters ACTG: \n");
 		String geneStart = sc.nextLine();
-		System.out.println("Input end gene: Must be 8 numbers and a config of letters ACTG:");
-		String geneEnd = sc.nextLine();
-  
-
-		System.out.println("Input a number:");
+		System.out.println("Input end gene: Must be 8 numbers and a config of letters ACTG: \n");
+		String geneEnd = sc.nextLine();  
+		System.out.println("Input a number: \n");
 		int num = sc.nextInt();
 		String[] banking = new String[num];
 		
 		for(int i = 0; i < banking.length; i++)
 		{
-			System.out.println("Input anouther gene:");
+			System.out.println("Input anouther gene: \n");
 			banking[i] = sc.nextLine();
 		}
 		
 		System.out.println(MinimumMutation(geneStart,geneEnd,banking));
 		sc.close(); 
 	}
-	
-	
-
 }
