f20cca4ea2d7a8c805fcc897104b98ca1b10e928
Dmaw25 <44574708+Dmaw25@users.noreply.github.com>
2018-12-17 10:34:06 -0500

    Code Challenge: Fetch
    Just a site about Quilts

---
+++ FetchChallenge/MakingAFetch/MakingAFetch/js/main.js
@@ -0,0 +1,39 @@
+﻿function createNode(element) {
+    return document.createElement(element);
+}
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+const apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=quilting';
+//const req = new Request(apiUrl);
+const ul = document.getElementById('MyLovelyBooks');
+
+fetch(apiUrl)
+    .then((resp) => resp.json())
+    .then(function (data) {
+        console.log(data);
+        let author = data.items;
+        return author.map(function (authors) {
+            let li = createNode('li'),
+                a = createNode('a'),
+                br = createNode('br'),
+                div = createNode('div'),
+                art = createNode('article'),
+                header = createNode('header');
+            a.setAttribute('href', `${authors.volumeInfo.canonicalVolumeLink}`, 'target', "_blank");
+            header.innerHTML = `${authors.volumeInfo.title}`;
+            div.innerHTML = `${authors.volumeInfo.authors}`;
+            art.innerHTML = `${authors.volumeInfo.description}`;
+            append(a, header);
+            append(li, a);
+            append(li, br);
+            append(a, div);
+            append(a, art);
+            append(ul, li);
+        })
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+// Your code here!
